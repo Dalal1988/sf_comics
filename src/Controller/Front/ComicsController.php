0d5938a737480820b31757e5b09f6283435dbaf1
khalid elhachimi <kh.elhachimi@gmail.com>
2022-01-14 01:23:33 +0100

    Initialisation

---
+++ src/Controller/Front/ComicsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\ComicsRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ComicsController extends AbstractController
+{
+
+    /**
+     * @Route("comics", name="comics_list")
+     */
+    public function comicsList(ComicsRepository $comicsRepository)
+    {
+        $comics = $comicsRepository->findAll();
+
+        return $this->render("front/comics.html.twig", ['comics' => $comics]);
+    }
+
+    /**
+     * @Route("comics/{id}", name="comics_show")
+     */
+    public function comicsShow($id, ComicsRepository $comicsRepository)
+    {
+        $comics = $comicsRepository->find($id);
+
+        return $this->render("front/comics.html.twig", ['comics' => $comics]);
+    }
+}
---
+++ src/Entity/Comics.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ComicsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ComicsRepository::class)
+ */
+class Comics
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $year;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Licence::class, inversedBy="comics")
+     */
+    private $licence;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Editor::class, inversedBy="comics")
+     */
+    private $editor;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Writer::class, inversedBy="comics")
+     */
+    private $writer;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Designer::class, inversedBy="comics")
+     */
+    private $designer;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Image::class, mappedBy="comics")
+     */
+    private $image;
+
+    public function __construct()
+    {
+        $this->Image = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getYear(): ?\DateTimeInterface
+    {
+        return $this->year;
+    }
+
+    public function setYear(\DateTimeInterface $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getLicence(): ?Licence
+    {
+        return $this->licence;
+    }
+
+    public function setLicence(?Licence $licence): self
+    {
+        $this->licence = $licence;
+
+        return $this;
+    }
+
+    public function getEditor(): ?Editor
+    {
+        return $this->editor;
+    }
+
+    public function setEditor(?Editor $editor): self
+    {
+        $this->editor = $editor;
+
+        return $this;
+    }
+
+    public function getWriter(): ?Writer
+    {
+        return $this->writer;
+    }
+
+    public function setWriter(?Writer $writer): self
+    {
+        $this->writer = $writer;
+
+        return $this;
+    }
+
+    public function getDesigner(): ?Designer
+    {
+        return $this->designer;
+    }
+
+    public function setDesigner(?Designer $designer): self
+    {
+        $this->designer = $designer;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Image[]
+     */
+    public function getImage(): Collection
+    {
+        return $this->image;
+    }
+
+    public function addImage(Image $image): self
+    {
+        if (!$this->image->contains($image)) {
+            $this->image[] = $image;
+            $image->setComics($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->Image->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getComics() === $this) {
+                $image->setComics(null);
+            }
+        }
+
+        return $this;
+    }
+}
